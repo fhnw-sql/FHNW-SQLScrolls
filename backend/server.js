e3a6c642a1aa8dbef94115efdacdb81f3997fe6f
Lucia de Espona <lucia.espona@gmail.com>
2021-10-19 13:04:30 +0200

    Added certificate. removed deprecated body-parser

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 ﻿require("rootpath")();
 require("dotenv").config();
+
+var fs = require('fs');
+var https = require('https');
+
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const jwt = require("./middlewares/jwt");
 const logger = require("./utils/logger");
 const db = require("./utils/db");
@@ -13,9 +15,16 @@ const errorHandler = require("./middlewares/errorHandler");
 // Check all environment variables
 validateEnvVars();
 
+// setup ssl
+var privateKey = fs.readFileSync(process.env.SSL_KEY || '/etc/ssl/private/nginx-selfsigned.key');
+var certificate = fs.readFileSync(process.env.SSL_CERT || '/etc/ssl/certs/nginx-selfsigned.crt');
+var credentials = {key: privateKey, cert: certificate};
+
+var app = express();
+
 // add BodyParser & Cors
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
 
 // use JWT auth to secure the api
@@ -36,9 +45,11 @@ app.use(errorHandler);
 const port = process.env.NODE_ENV === "production" ? process.env.PORT || 80 : process.env.PORT || 4000;
 
 // start server
+var httpsServer = https.createServer(credentials, app);
+
 if (!process.env.JEST_WORKER_ID) {
   db.connect().then((m) => {
-    app.listen(port, function () {
+    httpsServer.listen(port, function () {
       logger.info("Server listening on port " + port);
     });
   });
