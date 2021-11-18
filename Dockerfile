16dcdc37ede89dd547c5f8d63ac214653861f721
lucia.espona@fhnw.ch <lucia.espona@fhnw.ch>
2021-11-18 23:00:05 +0000

    fixes for switch aai on the docker setup

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,12 @@ WORKDIR /usr/share/nginx/html
 # Bundle app source
 COPY . .
 
+# set the nginx config
+#RUN nginx -t
+#RUN cat /etc/nginx/nginx.conf
+COPY ./nginx/nginx.conf /etc/nginx/nginx.conf
+
 # Replace API_URL
 RUN sed -i "s|https://fhnw-stg-api-web.dev.liqix.cc|${API_URL}|g" js/configuration.js
+
+
--- js/configuration.js
+++ js/configuration.js
@@ -2,4 +2,6 @@ Config = {
   API_URL: "https://fhnw-stg-api-web.dev.liqix.cc", // API HOST
   FALSE_ANSWER_UNTIL_MODEL_ANSWER: 4, // Count of answers until the model answer is provided
   EDITOR_PASSWORD: "fhnw", // Password for the editors.html
+  AUTH_COOKIE: "sqlscrolls-auth",
+  SWITCHAAI_LOGOUT_URL: "https://www.cs.technik.fhnw.ch/Shibboleth.sso/Logout", //usr to logout if using institutional login
 };
