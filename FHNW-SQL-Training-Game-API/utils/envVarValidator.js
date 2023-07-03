e74f839100472568295a5fdb074446d4c27e7e3b
janwilhelmfhnw <72554234+janwilhelmfhnw@users.noreply.github.com>
2023-07-03 16:05:56 +0200

    Add files via upload

---
+++ FHNW-SQL-Training-Game-API/utils/envVarValidator.js
@@ -0,0 +1,22 @@
+const logger = require("./logger");
+
+const environmentVariables = ["MONGODB_URI", "POSTMARK_API_KEY", "FROM_SENDER", "JWT_SECRET"];
+
+function setEnvVars() {
+  try {
+    require('dotenv').config();
+  } catch {}
+}
+
+function validateEnvVars() {
+  
+  setEnvVars()
+
+  let err = environmentVariables.filter((m) => !process.env[m]);
+  if (err.length > 0) {
+    logger.error(`Environment variables: ${err.join(",")} is not set!`);
+    throw new Error(`Environment variables:  ${err.join(",")}  should be set!`);
+  }
+}
+
+module.exports = validateEnvVars;
