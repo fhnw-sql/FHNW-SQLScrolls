1a56419e7b13d8b026d2911b8095f2e7db3388b1
Jan Wilhelm <jan.wilhelm@students.fhnw.ch>
2023-07-11 16:16:44 +0200

    uploaded github version

--- FHNW-SQL-Training-Game.github.io/js/configuration.js
+++
@@ -1,8 +0,0 @@
-Config = {
-  API_URL: "http://localhost:3000", // API HOST
-  FALSE_ANSWER_UNTIL_MODEL_ANSWER: 4, // Count of answers until the model answer is provided
-  FALSE_ANSWER_UNTIL_BOOK_HINT: 3, // Count of answers after which a hint references to the theory in the books
-  EDITOR_PASSWORD: "fhnw", // Password for the editors.html
-  AUTH_COOKIE: "sqlscrolls-auth",
-  SWITCHAAI_LOGOUT_URL: "https://www.cs.technik.fhnw.ch/Shibboleth.sso/Logout", //usr to logout if using institutional login
-};
